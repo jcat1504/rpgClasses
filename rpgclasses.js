8eb52f503e6699b326e0619a0ec2f92474f04b3f
Jess Y <31145424+jcat1504@users.noreply.github.com>
2018-05-20 19:48:26 -0700

    Add files via upload

---
+++ rpgclasses.js
@@ -0,0 +1,16 @@
+class Mage{
+  constructor(magicAttacks, protect){
+    this.magicAttacks = magicAttacks;
+    this.protect = protect;
+  }
+  get magicAttacks(){
+    return this.magicAttacks;
+  }
+  get protect(){
+    return this.protect;
+  }
+
+
+
+
+}
